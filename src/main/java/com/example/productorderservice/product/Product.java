28aee93f8339b3b7349b25b9f6f23c4f4701d245
choiseon <com1368@naver.com>
2023-02-19 14:19:18 +0900

    feat : 상품 조회 TDD 구현

--- src/main/java/com/example/productorderservice/product/Product.java
+++ src/main/java/com/example/productorderservice/product/Product.java
@@ -1,12 +1,24 @@
 package com.example.productorderservice.product;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final String name;
-    private final int price;
-    private final DiscountPolicy discountPolicy;
+    private String name;
+    private int price;
+    private DiscountPolicy discountPolicy;
 
     public Product(final String name, final int price, final DiscountPolicy discountPolicy) {
         Assert.hasText(name, "상품명은 필수입니다.");
@@ -15,13 +27,5 @@ class Product {
         this.name = name;
         this.price = price;
         this.discountPolicy = discountPolicy;
-    }
-
-    public void assignId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 }
